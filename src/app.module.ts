fe58e570baf1704dd32998294daeab0ddb09f411
tati <tatitaye0@gmail.com>
2024-03-04 15:35:41 +0300

    deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,19 +9,22 @@ import { Chat } from './chat/entities/chat.entity';
 import { Message } from './message/entities/message.entity';
 import { ChatModule } from './chat/chat.module';
 import { MessageModule } from './message/message.module';
+import * as dotenv from 'dotenv';
 
+// Load environment variables from .env file
+dotenv.config();
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'mysql',
-    host: 'localhost',//'mysql-159caac8-tatitaye0-03ac.a.aivencloud.com',
-    port: 3306,//26637,
-    username:'tati',//'avnadmin',  
-    password: '123',//'AVNS_luoZzR5b1SjmKg9dNor',  
-    database: 'chat',//'defaultdb',
-    entities: [User,Notification,Chat,Message],
+    host: 'chat-tatitaye0-03ac.a.aivencloud.com',
+    port: 26637,
+    username: 'avnadmin',
+    password: 'AVNS_6dd_jZ7SXQmYjqGhqAX',
+    database: 'defaultdb',
+    entities: [User, Notification, Chat, Message],
     synchronize: true,
-  }),UserModule, NotificationModule,ChatModule,MessageModule,],
+  }), UserModule, NotificationModule, ChatModule, MessageModule],
   controllers: [],
-  providers: [ NotificationGateway],
+  providers: [NotificationGateway],
 })
 export class AppModule {}
